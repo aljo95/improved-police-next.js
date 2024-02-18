d269f02d312bfaef2933fb27b7734317546175ae
aljo <johansson_a95@hotmail.com>
2024-02-18 21:00:47 +0100

    correct states and fetch method

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,57 @@
-
+'use client';
 
 import Link from 'next/link'
 import FilteringOptions from './components/FilteringOptions';
-import Todo from "./serverFetch"
+import { useEffect, useState } from 'react';
 
-async function getIncidents() {
-  const res = await fetch("https://polisen.se/api/events", {cache: "no-store"});
-  const incidents = await res.json();
+async function getIncidents(fetchQuery: string) {
+  let apiString: string = "https://polisen.se/api/events/";
+
+  /* Logic for fetching */  // /api/events/?location=visby
+  apiString = apiString + fetchQuery;
+
+  console.log("IN GETINCIS: " + apiString);
+
+  const res = await fetch(apiString, {cache: "no-store"});
+  const incidents = await res.json()
   return incidents;
 }
 
 
-export default async function Home( {posts}: any ) {
+export default function Home() {
 
-  //const incidents = await getIncidents();
-  console.log("-----");
-  //console.log(incidents);
-  console.log("-----");
+  const [fetchQuery, setFetchQuery] = useState<string>("");
+  const [listOfIncidents, setListOfIncidents] = useState<object[]>([]);
+  
+
+  useEffect(() => {
+    console.log("----WHEN----")
+    const incidents = getIncidents(fetchQuery);
+    incidents.then(res => setListOfIncidents(res));
+  }, [fetchQuery])
+
+  //console.log(typeof setFetchQuery); = Fuction | function ?
+
   return (
     <main className="border flex justify-center items-center flex-col">
-      <div>
+
+      { /*<Navloginbar />  startTime={startTime} setStartTime={setStartTime}
+        endTime={endTime} setEndTime={setEndTime}*/ }
+      <FilteringOptions 
+        fetchQuery={fetchQuery} setFetchQuery={setFetchQuery} 
         
-      </div>
+      />
 
-      { /*<Navloginbar /> */ }
-
-
-      <FilteringOptions />
-
-      <ul>
-      {/*incidents.map((todo: any) => (
-        <Todo key={todo.id} {...todo} />
-      ))*/}
-      </ul>
-
-
-      <h1>test</h1>
+      {listOfIncidents.map((info: any, index: number) => (
+        <p key={index} className="border">{info.summary}{info.location.name}{info.type}</p>
+      ))}
 
 
 
-      <h1><Link href="/users">Users</Link></h1> {/* for later */}
+
+
+      
 
     </main>
   )
-}
+ /* <h1><Link href="/users">Users</Link></h1> */ }
