4152ce7e5242cdf8d151786bd7d7b4224e7c1144
aljo <johansson_a95@hotmail.com>
2024-02-22 01:30:42 +0100

    typeboxes rendering and correct states

---
+++ app/components/TypeCheckboxes.tsx
@@ -0,0 +1,50 @@
+import React, { FC } from 'react'
+import { typesOfIncidents } from '../utils/types';
+
+interface IProps {
+    checkedState: boolean[],
+    setCheckedState: Function,
+}
+
+
+  // FC - type of functional component
+const TypeCheckboxes: FC<IProps> = (props: IProps):JSX.Element => {
+
+    const handleChange = (index1: number): void => {
+        const updateChecks = props.checkedState.map((elem, index2) => 
+            index1 === index2 ? !elem : elem
+        )       /* AM HERE */ 
+        props.setCheckedState(updateChecks);
+
+        /* query part happens in main filteringoptions */
+        
+    }
+
+
+  return (
+    <div>
+        <p>testtest</p>
+        {typesOfIncidents.types.map((element: any, index: number):JSX.Element => {
+            return (
+                <div id="checkboxes-container" key={index}>
+                    <input
+                    type="checkbox"
+                    id={`custom-checkbox-${index}`}
+                    name={element.typeOfCrime}
+                    value={element.typeOfCrime}
+                    checked={props.checkedState[index]}
+                    onChange={() => handleChange(index)}
+                    />
+                    <label htmlFor={`custom-checkbox-${index}`}>{element.typeOfCrime}</label>
+                </div>
+            )
+        }) 
+        }
+    
+    
+    
+    </div>
+  )
+}
+
+export default TypeCheckboxes
