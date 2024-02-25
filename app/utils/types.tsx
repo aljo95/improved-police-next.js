86939732696cc12e83e620d3b98104ce5eb89580
aljo <johansson_a95@hotmail.com>
2024-02-25 23:27:18 +0100

    all types + subtypes

--- app/utils/types.tsx
+++ app/utils/types.tsx
@@ -1,6 +1,7 @@
 
 interface Type {
     typeOfCrime: string;
+    valueString: string;
 }
 
 interface TypeCollection {
@@ -10,10 +11,49 @@ interface TypeCollection {
 export const typesOfIncidents: TypeCollection = {
     types: [
         {
-            typeOfCrime: "Misshandel"
+            typeOfCrime: "Brand-Brand automatlarm",
+            valueString: "Brand",
         },
         {
-            typeOfCrime: "Trafikbrott"
-        }
-    ]   /* adding more soon */
+            typeOfCrime: "Misshandel-Bråk-Misshandel, grov",
+            valueString: "Bråk, Misshandel",
+        },
+        {
+            typeOfCrime: `Inbrott-Inbrott, försök-Rån-Rån väpnat-Rån övrigt-Rån, försök-
+            Stöld-Stöld, försök-Stöld, ringa-Stöld/inbrott-Larm inbrott`,
+            valueString: "Inbrott, Stöld, Rån",
+        },
+        {
+            typeOfCrime: "Mord/dråp-Mord/dråp, försök",
+            valueString: "Mord"
+        },
+        {
+            typeOfCrime: `Trafikbrott-Trafikhinder-Trafikkontroll-Trafikolycka-Trafikolycka
+            , personskada-Trafikolycka, singel-Trafikolycka, smitning från-Trafikolycka, vilt`,
+            valueString: "Trafikbrott och olyckor",
+        },
+        {
+            typeOfCrime: "Larm överfall-Våldtäkt-Våldtäkt, försök",
+            valueString: "Överfall & Våldtäkt"
+        },
+        {
+            typeOfCrime: `Sammanfattning dag-Sammanfattning dygn-Sammanfattning eftermiddag
+            -Sammanfattning förmiddag-Sammanfattning helg-Sammanfattning kväll-Sammanfattning kväll och natt
+            -Sammanfattning natt-Sammanfattning vecka`,
+            valueString: "Sammanfattningar",
+        },
+        {
+            typeOfCrime: `Alkohollagen-Anträffad död-Anträffat gods-Arbetsplatsolycka-Bedrägeri-Bombhot
+            -Detonation-Djur skadat/omhändertaget-Ekobrott-Farligt föremål, misstänkt-Fjällräddning
+            -Fylleri/LOB-Förfalskningsbrott-Försvunnen person-Gränskontroll-Häleri-Knivlagen-Kontroll person/fordon
+            -Lagen om hundar och katter-Miljöbrott-Missbruk av urkund
+            -Motorfordon, anträffat stulet-Motorfordon, stöld-Narkotikabrott-Naturkatastrof
+            -Ofog barn/ungdom-Ofredande/förargelse-Olaga frihetsberövande-Olaga hot-Olaga intrång/hemfridsbrott
+            -Olovlig körning-Ordningslagen-Polisinsats/kommendering-Rattfylleri-Räddningsinsats-Sedlighetsbrott
+            -Sjukdom/olycksfall-Sjölagen-Skadegörelse-Skottlossning-Skottlossning, misstänkt-Spridning smittsamma kemikalier
+            -Tillfälligt obemannat-Uppdatering-Utlänningslagen-Vapenlagen-Varningslarm/haveri-Våld/hot mot tjänsteman
+            -Vållande till kroppsskada`,
+            valueString: "Övrigt",
+        },
+    ] 
 }
